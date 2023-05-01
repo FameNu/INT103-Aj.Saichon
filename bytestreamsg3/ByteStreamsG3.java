7565a71a3b23a2e8a22495fe6ec88881a18124fc
FameNu <110712106+FameNu@users.noreply.github.com>
2023-05-01 14:57:51 +0700

    Delete bytestreamsg3 directory

--- bytestreamsg3/ByteStreamsG3.java
+++
@@ -1,8 +0,0 @@
-package bytestreamsg3;
-
-public class ByteStreamsG3 {
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
--- bytestreamsg3/Shape.java
+++
@@ -1,5 +0,0 @@
-package bytestreamsg3;
-
-public interface Shape {
-    public void draw();
-}
